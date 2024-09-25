43a8c788f97e161e013fa05f8a3cb3e1bec43172
Myles Pritchett <myles@unicard.com.au>
2024-09-25 15:49:38 +1000

    render as dynamic for recipes

--- src/app/recipes/[recipe-name]/page.tsx
+++ src/app/recipes/[recipe-name]/page.tsx
@@ -1,24 +1,5 @@
 import { notFound } from 'next/navigation';
 import { prisma } from "@/lib/prisma";
-import { Recipe, RecipeIngredient } from '@prisma/client';
-
-// Function to fetch all recipes with ingredients
-async function fetchRecipes(): Promise<Recipe[]> {
-  try {
-    return await prisma.recipe.findMany({
-      include: {
-        recipeIngredients: {
-          include: {
-            ingredient: true,
-          },
-        },
-      },
-    });
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-    return [];
-  }
-}
 
 // Function to fetch a specific recipe by its slug name
 async function fetchRecipeByName(slugName: string) {
@@ -53,29 +34,13 @@ function decodeRecipeName(slug: string | undefined): string {
   return slug.replace(/-/g, ' '); // Decode the slug to match the recipe name
 }
 
-// Step 1: Generate static params for the dynamic routes
-export async function getStaticPaths() {
-  const recipes = await fetchRecipes();
-
-  const paths = recipes.map((recipe) => ({
-    params: { 'recipe-name': recipe.name.replace(/ /g, '-') },
-  }));
-
-  return {
-    paths,
-    fallback: 'true', // or 'true' if you want to serve new recipes dynamically
-  };
-}
-
-// Step 3: Default page component
+// Default page component
 export default async function RecipePage({ params }: { params: { 'recipe-name': string } }) {
   const slug = params['recipe-name'];
-  console.log("Slug from params:", slug);
-
   const recipe = await fetchRecipeByName(slug);
 
   if (!recipe) {
-    notFound();
+    notFound(); // Show 404 if recipe not found
   }
 
   // Ensure recipeIngredients is defined before checking its length
